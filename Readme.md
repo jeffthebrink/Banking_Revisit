0257b2b25fc1e6d513c1a19b3e1133e97b61b43b
jeffthebrink <jeffthebrink@gmail.com>
2017-08-30 16:10:04 -0600

    initial commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+#Banking Revisit
+
+This is a revisit to the banking project. This is similar to the ATM project, but instead allows the
+creation of multiple users with multiple accounts. 
---
+++ src/Main.java
@@ -0,0 +1,123 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+
+    static HashMap<String, Double> accountList = new HashMap();
+
+    public static void checkBalance(String userName) {
+        if (accountList.get(userName) != null) {
+            System.out.println("Your current balance is: $" + accountList.get(userName));
+        } else {
+            System.out.println("You must be logged in to view a balance!");
+        }
+    }
+
+    public static void withdraw(String userName) {
+        if (accountList.containsKey(userName)) {
+            Scanner scannerDouble = new Scanner(System.in);
+            System.out.println("How much would you like to withdraw?");
+            Double withdrawAmt = scannerDouble.nextDouble();
+            if (withdrawAmt > accountList.get(userName)) {
+                System.out.println("You cannot withdraw more than your balance!");
+            } else {
+                Double currentBalance = accountList.get(userName);
+                Double newBalance = currentBalance - withdrawAmt;
+                accountList.put(userName, newBalance);
+                System.out.println("Your new balance is: $" + accountList.get(userName));
+            }
+        } else {
+            System.out.println("You must be logged in to withdraw money!");
+        }
+    }
+
+    public static void closeAccount(String userName) {
+        accountList.remove(userName);
+        System.out.println("Account removed.");
+    }
+
+    public static void depositFunds(String userName) {
+        Scanner scannerDouble = new Scanner(System.in);
+        System.out.println("How much would you like to deposit?");
+        Double depositAmt = scannerDouble.nextDouble();
+        Double currentBalance = accountList.get(userName);
+        Double newBalance = depositAmt + currentBalance;
+        accountList.put(userName, newBalance);
+        System.out.println("Your new balance is: $" + accountList.get(userName));
+    }
+
+    public static void chooseOption(String userName) {
+        System.out.println();
+        System.out.println("Please choose an option below: ");
+        System.out.println("1. Check balance");
+        System.out.println("2. Withdraw money");
+        System.out.println("3. Close account");
+        System.out.println("4. Deposit funds");
+        System.out.println("5. Exit system");
+
+        Scanner scannerInt = new Scanner(System.in);
+        Integer option = scannerInt.nextInt();
+
+        switch (option) {
+            case 1:
+                checkBalance(userName);
+                break;
+            case 2:
+                withdraw(userName);
+                break;
+            case 3:
+                closeAccount(userName);
+                break;
+            case 4:
+                depositFunds(userName);
+                break;
+            case 5:
+                System.out.println("Exiting system...");
+                break;
+            default:
+                System.out.println("You must input a valid option!");
+                break;
+        }
+
+    }
+
+    public static void createNewAccount(String userName) {
+        Scanner scannerDouble = new Scanner(System.in);
+        System.out.println("Creating new account.");
+        System.out.println("How much would you like to deposit into your new account?");
+        Double depositAmt = scannerDouble.nextDouble();
+        accountList.put(userName, depositAmt);
+        System.out.println("Your new account balance is: $" + accountList.get(userName));
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello and welcome to the Bank!");
+        System.out.println();
+        System.out.println("Please enter your name to begin banking.");
+
+        String userName = scanner.nextLine();
+
+        if (accountList.containsKey(userName)) {
+            while (true) {
+                chooseOption(userName);
+            }
+        } else {
+            System.out.println("No user name found. Would you like to create an account? y/n?");
+            String createAccount = scanner.nextLine().toLowerCase();
+            if (createAccount.equals("y")) {
+                createNewAccount(userName);
+                while (true) {
+                    chooseOption(userName);
+                }
+            } else {
+                System.out.println("You have selected to not create an account. Goodbye!");
+                System.exit(1);
+            }
+        }
+
+    }
+}
